93951c807ae8c3256b1481befe8c3cadb1dc8fbf
Joann Alfante <joann.alfante@cit.edu>
2023-11-01 18:22:08 +0800

    made changes to home/login

---
+++ src/pages/home/styles.jsx
@@ -0,0 +1,34 @@
+import { styled } from '@mui/material/styles';
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,700;1,100&display=swap');
+</style>
+
+export const ButtonStyle1 = {
+    backgroundColor: "#fecc00",
+    borderColor: "#fecc00",
+    fontWeight: "bold",
+    color: "black",
+    ":hover": {
+      bgcolor: "#9c7b16",
+      color: "white",
+    },
+    width: "300px"
+  };
+
+export const loginstyle = {
+    marginTop: "30px",
+    position: "absolute",
+    boxShadow: '1px 2px 9px #C0C0C0',
+    borderRadius: "15px",
+    marginLeft: "450px",
+   
+}
+
+export const fields = {
+    width: "300px"
+}
+
+export const logo = {
+    marginTop: 50,
+}
+
